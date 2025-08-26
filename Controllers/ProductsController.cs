85f34a02c099a14ba94ea4f63bcc0f5d895b714c
siddabathulajagadeeshbabu <siddabathulajagadeeshbabu@gmail.com>
2025-08-26 09:21:49 +0530

    hello

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -1,21 +1,45 @@
+using System.Data;
 using Microsoft.AspNetCore.Mvc;
-using MvcDemo.Models; //assuming Models namespace contains Product model
+using System.Data.SqlClient;
+using MvcADO_Demo.Data; // Import the namespace where ProductsRepository is defined 
+       
 
-namespace MvcDemo.Controllers
+namespace MvcADO_Demo.Controllers
 {
     public class ProductsController : Controller
-    { //controllers are responsible for handling user requests and returning responses
-
-        //get: /Products/ Index
+    {
+        private readonly ProductsRepository _productsRepository;
+        //Above variable holds the reference to the ProductsRepository instance
+        public ProductsController(ProductsRepository productsRepository)
+        {
+            _productsRepository = productsRepository;
+        }
+        // Action methods go here
         public IActionResult Index()
         {
-            var products = new List<Product>
+            var productsDataSet = _productsRepository.GetProducts();
+            //invoking getProducts method for retrieving all products from the database
+            return View(productsDataSet);
+            // The view will be able to access the DataSet and display the products
+            //here we can use viewdata as well as viewbag
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+   [HttpPost]
+        public IActionResult Create(string name, decimal price)
+        {
+            //here we can call insertproduct deined inrepository class with a condition that price> = 0 and model is valid
+            if (price >= 0 && ModelState.IsValid)
+            // Check if price is valid and model state is valid that means if 
+            // model is not returning any error 
             {
-                new Product { id = 1, Name = "Laptop", Price = 10.00m },
-                new Product { id = 2, Name = "Mouse", Price = 20.00m },
-                new Product { id = 3, Name = "Printer", Price = 30.00m }
-            };
-            return View(products); //passing products to the view
+                var newProductId = _productsRepository.InsertProduct(name, price);
+                return RedirectToAction("Index");
+            }
+            return View();
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,8 +1,17 @@
+using System.Data.SqlClient;
+using System.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+
+//Register our ADO.NET services
+builder.Services.AddScoped<IDbConnection>(db => new SqlConnection (builder.Configuration.GetConnectionString("Default")));
+//Above service integration allows us to inject IDbConnection into our controllers hence 
+//we can access the database easily.
+builder.Services.AddScoped<MvcADO_Demo.Data.ProductsRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -20,10 +29,10 @@ app.UseAuthorization();
 
 app.MapStaticAssets();
 
+//Default route to product lists
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
+    pattern: "{controller=Products}/{action=Index}/{id?}")
     .WithStaticAssets();
-
 
 app.Run();
